4fb1efe955943191065cc138150fb2b8e8576a7c
SirTonyEdgar <121780770+SirTonyEdgar@users.noreply.github.com>
2024-03-16 09:13:35 +0700

    Add files via upload

---
+++ jurnalmod4/jurnalmod4/PosisiKarakterGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod4
+{
+    internal class PosisiKarakterGame
+    {
+        enum State { Tengkurap, Jongkok, Berdiri, Terbang};
+        public static void Main()
+        {
+            State state = State.Berdiri;
+            
+        }
+    }
+}
+
---
+++ jurnalmod4/jurnalmod4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah kodebuah = new KodeBuah();
+
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Ceri));
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Apel));
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Melon));
+        }
+    }
+}
